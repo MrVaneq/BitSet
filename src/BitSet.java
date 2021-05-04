dd25f002f5b0da65eaa5db2cc4d3dbd03a92141f
MrVaneq <potekhin.vanya@mail.ru>
2021-05-05 00:53:50 +0300

    2

--- src/BitSet.java
+++ src/BitSet.java
@@ -1,114 +1,98 @@
 import java.util.Arrays;
 
-public class BitSet<T> {
+class BitSet {
+
+    private int[] array; //elements
     private int size;
-    private Object[] elements;
+    private final int intSize = Integer.SIZE; //32
 
     public BitSet(int size) {
         this.size = size;
-        elements = new Object[size];
-        for (int i = 0; i < size; i++) {
-            elements[i] = null;
+
+        if (size % intSize == 0) {
+            array = new int[size / intSize];
+        } else {
+            array = new int[(size / intSize) + 1];
+        }
+
+        for (int i = 0; i < size / intSize; i++) {
+            array[i] = 0;
         }
     }
 
-    public void add(T o) {
-        if (o != null) {
-            for (int i = 0; i < size; i++) {
-                if (elements[i] == null) {
-                    elements[i] = o;
-                    break;
+    public void add(int element) {
+        if (element <= size) {
+            int num = element % intSize;
+            int intNum = 1;
+            if (element % intSize != 0) {
+                element = element / (intSize + 1);
+                intNum = intNum << (intSize - num);
+            } else {
+                element = (element / intSize) - 1;
+            }
+            array[element] |= intNum;
+        }
+    }
+
+    public void remove(int element) {
+        if (element <= size) {
+            int num = element % intSize;
+            int intNum = Integer.MAX_VALUE - 1;
+            if (element % intSize != 0) {
+                element = element / intSize;
+                for (int i = 0; i < intSize - num; i++) {
+                    intNum <<= (1);
+                    intNum |= 1;
                 }
-                if (i == size - 1) throw new IllegalArgumentException("Bitset is full");
+            } else {
+                element = (element / intSize) - 1;
             }
+            array[element] &= intNum;
         }
     }
 
-    public void add(Object[] objects) {
-        for (Object object : objects) {
-            for (int j = 0; j < size; j++) {
-                if (elements[j] == null) {
-                    elements[j] = object;
-                    break;
-                }
-                if (j == size - 1) throw new IllegalArgumentException("Bitset is full");
-            }
-        }
-    }
+    public BitSet union(BitSet set) {
+        BitSet unioned = new BitSet(Math.max(size, set.size));
+        int setLength = Math.min(array.length, set.array.length);
 
-    public void remove(T o) {
-        for (int i = 0; i < size; i++) {
-            if (o.equals(elements[i])) {
-                elements[i] = null;
-            }
-        }
-    }
-
-    public void remove(Object[] objects) {
-        for (Object object : objects) {
-            for (int j = 0; j < size; j++) {
-                if (elements[j] != null && object != null) {
-                    if (elements[j].equals(object)) {
-                        elements[j] = null;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    public void remove(int i) {
-        elements[i] = null;
-    }
-
-    public BitSet<T> addition(BitSet second) {
-        BitSet<T> additioned = new BitSet(size);
-        additioned.add(elements);
-        for (Object o1 : getElements()) {
-            for (Object o2 : second.getElements()) {
-                if (o1.equals(o2)) {
-                    additioned.remove((T) o1);
+        for (int i = 0; i < unioned.array.length; i++) {
+            if (i < setLength) {
+                unioned.array[i] = array[i] | set.array[i];
+            } else {
+                if (setLength == array.length) {
+                    unioned.array[i] = set.array[i];
+                } else {
+                    unioned.array[i] = array[i];
                 }
             }
-        }
-        return additioned;
-    }
-
-    public BitSet<T> union(BitSet second) {
-        BitSet unioned = new BitSet(second.size + size);
-        for (Object o : getElements()) {
-            if (!unioned.contains(o)) unioned.add(o);
-        }
-        for (Object o : second.getElements()) {
-            if (!unioned.contains(o)) unioned.add(o);
         }
         return unioned;
     }
 
-    public BitSet<T> intersect(BitSet second) {
-        BitSet<T> intersected = new BitSet<>(size + second.size);
-        for (Object o1 : getElements()) {
-            for (Object o2 : second.getElements()) {
-                if (o1 != null && o2 != null) {
-                    if (!o1.getClass().equals(o2.getClass())) throw new IllegalArgumentException("Different raw types");
-                    if (o1.equals(o2)) {
-                        intersected.add((T) o1);
-                    }
-                }
-            }
+    public BitSet intersect(BitSet set) {
+        BitSet intersected = new BitSet(Math.min(size, set.size));
+        for (int i = 0; i < intersected.array.length; i++) {
+            intersected.array[i] = array[i] & set.array[i];
         }
         return intersected;
     }
 
-    public boolean contains(T o) {
-        if (o == null) return false;
-        for (Object obj : getElements()) {
-            if (obj != null) {
-                if (obj.equals(o)) return true;
-
-            }
+    public BitSet complement() {
+        BitSet complemented = new BitSet(size);
+        for (int i = 0; i < size / intSize; i++) {
+            complemented.array[i] = ~array[i];
         }
-        return false;
+        return complemented;
+    }
+
+    public boolean contains(int element) {
+        int temp = array[element / (intSize + 1)];
+        temp >>= (intSize - (element % intSize));
+        return 1 == temp % 2;
+    }
+
+    public int[] getArray() {
+        return array;
     }
 
     @Override
@@ -116,46 +100,11 @@ public class BitSet<T> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         BitSet bitset = (BitSet) o;
-        return size == bitset.size && Arrays.equals(elements, bitset.elements);
+        return size == bitset.size && Arrays.equals(array, bitset.array);
     }
 
     @Override
     public int hashCode() {
-        return 31 * size + Arrays.hashCode(elements);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder answer = new StringBuilder();
-        for (Object o : getElements()) {
-            if (o != null) {
-                String appended;
-                appended = o.toString();
-                answer.append(appended).append("\n");
-            }
-        }
-        return answer.toString();
-    }
-
-    public Object[] getElements() {
-        return elements;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public T get(int i) {
-        return (T) elements[i];
-    }
-
-    public int elementsSize() {
-        int answer = 0;
-        for (Object o : elements) {
-            if (o != null) {
-                answer++;
-            }
-        }
-        return answer;
+        return 31 * size + Arrays.hashCode(array);
     }
 }
--- test/tests.java
+++ test/tests.java
@@ -2,98 +2,116 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class tests {
+class BitSetTest {
 
     @Test
-    public void remove() {
-        BitSet<String> bitset1 = new BitSet<>(7);
-        bitset1.add("fff");
-        bitset1.add("aaa");
-        bitset1.add("ppp");
-        bitset1.add("zzz");
-        bitset1.add("nnn");
-        bitset1.remove("ppp");
-        bitset1.remove(3);
+    void add() {
+        BitSet set = new BitSet(320);
+        set.add(32 * 2);
+        set.add(32 * 5);
+        set.add(32 * 6);
+        set.add(32 * 9);
 
-        assertFalse(bitset1.contains("ppp"));
-        assertFalse(bitset1.contains("zzz"));
-        assertArrayEquals(new Object[]{"fff", "aaa", null, null, "nnn", null, null}, bitset1.getElements());
-    }
-
-    @Test
-    public void add() {
-        BitSet<String> bitset1 = new BitSet<>(5);
-        bitset1.add("first");
-        bitset1.add("hi");
-        bitset1.add("hello");
-        bitset1.add("test");
-        bitset1.add("last");
-
-        assertEquals(5, bitset1.getSize());
-        assertEquals("last", bitset1.get(4));
-        assertEquals("hello", bitset1.get(2));
-        assertTrue(bitset1.contains("test"));
-        assertArrayEquals(new Object[]{"first", "hi", "hello", "test", "last"}, bitset1.getElements());
-
-        bitset1 = new BitSet<>(10);
-        bitset1.add(new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"});
-        assertArrayEquals(new Object[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}, bitset1.getElements());
-        bitset1.remove(new String[]{"3", "4", "5", "7", "9"});
-        assertArrayEquals(new Object[]{"0", "1", "2", null, null, null, "6", null, "8", null}, bitset1.getElements());
-        assertEquals(5, bitset1.elementsSize());
-    }
-
-    @Test
-    public void union() {
-        BitSet<String> bitset1 = new BitSet<>(6);
-        BitSet<String> bitset2 = new BitSet<>(6);
-        bitset1.add("aaa");
-        bitset1.add("oooo");
-        bitset1.add("ppp");
-
-        bitset2.add("123");
-        bitset2.add("ppp");
-        bitset2.add("hhh");
-
-        Object[] expected = new Object[]{"aaa", "oooo", "ppp", "123", "hhh", null, null, null, null, null, null, null};
-        Object[] actual = bitset1.union(bitset2).getElements();
+        int[] expected = {0, 1, 0, 0, 1, 1, 0, 0, 1, 0};
+        int[] actual = set.getArray();
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void intersect() {
-        BitSet<String> bitset1 = new BitSet<>(4);
-        BitSet<String> bitset2 = new BitSet<>(4);
-        bitset1.add("hi");
-        bitset1.add("hello");
-        bitset1.add("i");
+    void remove() {
+        BitSet set = new BitSet(352);
+        set.add(32);
+        set.add(32 * 2);
+        set.add(32 * 3);
+        set.add(32 * 6);
+        set.add(32 * 11);
+        set.remove(32 * 6);
 
-        bitset2.add("666");
-        bitset2.add("123");
-        bitset2.add("hi");
-
-        Object[] expected = new Object[]{"hi", null , null, null, null, null, null, null};
-        Object[] actual = bitset1.intersect(bitset2).getElements();
+        int[] expected = {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1};
+        int[] actual = set.getArray();
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void addition() {
-        BitSet<String> bitset1 = new BitSet<>(5);
-        BitSet<String> bitset2 = new BitSet<>(5);
-        bitset1.add("q");
-        bitset1.add("w");
-        bitset1.add("e");
-        bitset1.add("r");
-        bitset1.add("t");
+    void union() {
+        BitSet set1 = new BitSet(320);
+        BitSet set2 = new BitSet(320);
+        set1.add(32);
+        set1.add(32 * 4);
+        set1.add(32 * 6);
+        set1.add(32 * 8);
+        set1.add(32 * 9);
+        set2.add(32);
+        set2.add(32 * 5);
+        set2.add(32 * 6);
+        set2.add(32 * 8);
+        set2.add(32 * 10);
 
-        bitset2.add("e");
-        bitset2.add("r");
-        bitset2.add("t");
-
-        Object[] expected = new Object[]{"q", "w", null , null, null};
-        Object[] actual = bitset1.addition(bitset2).getElements();
+        BitSet set3 = set1.union(set2);
+        int[] expected = {1, 0, 0, 1, 1, 1, 0, 1, 1, 1};
+        int[] actual = set3.getArray();
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void intersect() {
+        BitSet set1 = new BitSet(320);
+        BitSet set2 = new BitSet(320);
+        set1.add(32);
+        set1.add(32 * 4);
+        set1.add(32 * 6);
+        set1.add(32 * 8);
+        set1.add(32 * 9);
+        set2.add(32);
+        set2.add(32 * 5);
+        set2.add(32 * 6);
+        set2.add(32 * 8);
+        set2.add(32 * 10);
+
+        BitSet set3 = set1.intersect(set2);
+        int[] expected = {1, 0, 0, 0, 0, 1, 0, 1, 0, 0};
+        int[] actual = set3.getArray();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void complement() {
+        BitSet set1 = new BitSet(320);
+        BitSet set2 = new BitSet(320);
+        set1.add(32);
+        set1.add(32 * 3);
+        set1.add(32 * 4);
+        set1.add(32 * 6);
+        set2.add(32);
+        set2.add(32 * 2);
+        set2.add(32 * 3);
+        set2.add(32 * 6);
+        set2.add(32 * 9);
+
+        BitSet set3 = set1.complement();
+        BitSet set4 = set2.complement();
+
+        int[] expected1 = {-2, -1, -2, -2, -1, -2, -1, -1, -1, -1};
+        int[] actual1 = set3.getArray();
+        int[] expected2 = {-2, -2, -2, -1, -1, -2, -1, -1, -2, -1};
+        int[] actual2 = set4.getArray();
+        assertArrayEquals(expected1, actual1);
+        assertArrayEquals(expected2, actual2);
+    }
+
+    @Test
+    void contains() {
+        BitSet set1 = new BitSet(320);
+        set1.add(32);
+        set1.add(32 * 3);
+        set1.add(32 * 4);
+        set1.add(32 * 5);
+        set1.add(32 * 6);
+
+        assertTrue(set1.contains(32 * 3));
+        assertTrue(set1.contains(32 * 4));
+        assertFalse(set1.contains(32 * 2));
+        assertFalse(set1.contains(32 * 9));
     }
 
 }
